e8ac6fcab16e4d3a413ed68a38cd57097a0b9268
Witold Brzezinski <witbrzezinski@gmail.com>
2022-05-12 17:48:38 +0200

    initial commit

---
+++ src/test/java/pl/witoldbrzezinski/SocialNetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.witoldbrzezinski.SocialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
