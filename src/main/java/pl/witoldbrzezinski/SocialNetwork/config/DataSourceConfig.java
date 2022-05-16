08cc1995351dd8752eeefb189e8a6afec784acea
Witold Brzezinski <witbrzezinski@gmail.com>
2022-05-16 19:53:32 +0200

    added data source configuration & added sql script creating basic user info

---
+++ src/main/java/pl/witoldbrzezinski/SocialNetwork/config/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package pl.witoldbrzezinski.SocialNetwork.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driverClassName);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+
+
+}
