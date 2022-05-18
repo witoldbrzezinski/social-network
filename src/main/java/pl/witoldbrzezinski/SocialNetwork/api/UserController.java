a1b8d5d815b84dc5a057f3043f29db02d54152e4
Witold Brzezinski <witbrzezinski@gmail.com>
2022-05-18 23:50:02 +0200

    added rest contoller for users with some CRUD methods

---
+++ src/main/java/pl/witoldbrzezinski/SocialNetwork/api/UserController.java
@@ -0,0 +1,50 @@
+package pl.witoldbrzezinski.SocialNetwork.api;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.witoldbrzezinski.SocialNetwork.entity.AppUser;
+import pl.witoldbrzezinski.SocialNetwork.entity.Role;
+import pl.witoldbrzezinski.SocialNetwork.service.AppUserService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final AppUserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<AppUser>>getUsers(){
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUser>saveUser(@RequestBody AppUser user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addToUser")
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form){
+        userService.addRoleToUser(form.getUsername(),form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
+}
